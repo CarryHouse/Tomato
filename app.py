fb57a8f3e63cbb1d8e66e8118454194a2154a6ac
tobiusaolo <tobiusaolo@github.com>
2022-01-27 18:00:28 +0300

    added data

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ async def create_data(item:Pointdata):
 @app.get("/fetchdata/")
 async def read_results():
     try:
-        query = "select * from Traffic"
+        query = "select * from Traffic order by trackdate DESC"
         result = await database.fetch_all(query=query)
     except:
         result= {"Error":"Database is empty"}
